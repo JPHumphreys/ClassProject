0189108d4a97505ea39162be2d87f5423afd7dc1
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-09-10 18:37:37 +0100

    added a readline

--- ClassProject/Program.cs
+++ ClassProject/Program.cs
@@ -6,7 +6,8 @@ namespace ClassProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
